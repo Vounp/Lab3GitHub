e2fb8381a7be286e4e6029c14395c4b6412460ca
Passoret Vounba <v.passoret@gmail.com>
2024-05-22 07:50:24 -0400

    new_test

---
+++ getting_ready_op2.R
@@ -0,0 +1,6 @@
+#Downloading data
+new_data <- read.csv("/Users/vpass/OneDrive/Bureau/Cours_Pablo/train_.csv")
+summary(new_data)
+install.packages("ggplot2")
+install.packages("ggplot")
+new_data %% ggplot(aes(x=x0))+geom_histogram()
